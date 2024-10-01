b8c031f391836245efda7d9823dc8a4960613ed2
Laura <deboragonzalez1975@gmail.com>
2024-10-01 01:23:33 -0300

    Problema resuelto.....modifique tsconfig

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,14 @@
 import { Sequelize, DataTypes } from "sequelize";
 
-const sequelize = new Sequelize(
-  "postgresql://ADA_owner:IZ30WhxVumqA@ep-tiny-river-a5qu19px.us-east-2.aws.neon.tech/ADA?sslmode=require"
-);
+const sequelize = new Sequelize(`${process.env.DB_URI}`);
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
 
+export { DataTypes };
 export default sequelize;
